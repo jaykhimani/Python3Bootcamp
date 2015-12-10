da59047aa3345fa363dda889f557880265141a71
Jay Khimani <jay.khimani@gmail.com>
2015-12-10 21:57:10 +0100

    Adding exercise 3

---
+++ bootcamp/ex3_function.py
@@ -0,0 +1,50 @@
+__author__ = 'jay'
+
+
+def cheapest_paperback(mybooks):
+    lowest_price = -1
+    lowest_price_book = None
+    for book in mybooks.keys():
+        paperback = mybooks[book]['formats']['paperback']
+        consumer_price = paperback['price'] + paperback['shipping']
+        if lowest_price == -1: lowest_price = consumer_price
+        if consumer_price < lowest_price: lowest_price_book = book
+    return {"book": lowest_price_book, "consumer_price": consumer_price}
+
+
+books = {'The Hitchhikers Guide to the Galaxy':
+             {'author': 'Douglas Adams',
+              'formats': {'ebook': {'price': 12.30,
+                                    'shipping': 0.00},
+                          'hardcopy': {'price': 20.00,
+                                       'shipping': 2.50},
+                          'paperback': {'price': 15.75,
+                                        'shipping': 1.50}}},
+         '2 The Hitchhikers Guide to the Galaxy':
+             {'author': 'Douglas Adams',
+              'formats': {'ebook': {'price': 12.30,
+                                    'shipping': 0.00},
+                          'hardcopy': {'price': 20.00,
+                                       'shipping': 2.50},
+                          'paperback': {'price': 15.76,
+                                        'shipping': 1.50}}},
+         'The Three Musketeers': {'author': 'Alexandre Dumas',
+                                  'formats': {'ebook': {'price': 19.90,
+                                                        'shipping': 0.00},
+                                              'hardcopy': {'price': 38.00,
+                                                           'shipping': 3.00},
+                                              'paperback': {'price': 24.10,
+                                                            'shipping':
+                                                                2.00}}},
+         '12 The Three Musketeers': {'author': 'Alexandre Dumas',
+                                     'formats':
+                                         {'ebook': {'price': 19.90,
+                                                    'shipping': 0.00},
+                                          'hardcopy': {'price': 38.00,
+                                                       'shipping': 3.00},
+                                          'paperback': {'price': 24.10,
+                                                        'shipping': 2.00}}}}
+
+result = cheapest_paperback(books)
+print("%s is the cheapest book priced at %f" %
+      (result['book'], result['consumer_price']))

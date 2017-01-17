e1913c86c9330595a44255bc72bcce48c6c43ccf
Jay Khimani <jay.khimani@gmail.com>
2017-01-17 09:25:23 +0100

    Adding algorithms implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python3Bootcamp
-Python 3 boot camp based on [Karel's Python boot camp lecture](http://kubat.nl/pages/lectures)
+This is a playground/scratchpad to learn and play around Python 3 
+* `bootcamp` directory is based on [Karel's Python boot camp lecture](http://kubat.nl/pages/lectures)
+* `algorithms` directory as name suggest is implementation of various algorithms in python.
---
+++ algorithms/bubble_sort/bubblesort.py
@@ -0,0 +1,35 @@
+import time
+
+__author__ = 'jay'
+
+
+def bubble_sort(some_list: list):
+    """
+    Sorts the provided list using bubble sort algorithm
+
+    Parameters
+    -----------
+    some_list: list
+        List which will be sorted
+    """
+    total_sorted = 0
+    idx = list_len = len(some_list)
+    while total_sorted < list_len - 1:
+        while idx >= 2 + total_sorted:
+            num2 = some_list[idx - 1]
+            num1 = some_list[idx - 2]
+            if num2 < num1:
+                some_list[idx - 1] = num1
+                some_list[idx - 2] = num2
+            idx -= 1
+
+        total_sorted += 1
+        idx = list_len
+
+
+my_list = [2, 3, 1, 2, 3, 1, 56, 23, 1, 232, 11, 100, 232]
+
+start_time = time.clock()
+bubble_sort(my_list)
+print(my_list)
+print("Time taken %f sec" %(time.clock() - start_time))
